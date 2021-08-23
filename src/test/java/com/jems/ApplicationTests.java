31a03da5dfd945339a656f46fcc5341c145c7cf6
Lalmasthan <33448997+Lalmasthan@users.noreply.github.com>
2021-08-23 14:59:38 +0530

    Add files via upload
    spring boot test

---
+++ src/test/java/com/jems/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
